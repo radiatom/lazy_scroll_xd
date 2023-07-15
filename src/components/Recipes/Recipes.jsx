e3f40890a75447dc5b0970e5c0532aef242e8bc5
radiatom <arssenmozol@gmail.com>
2023-07-15 13:56:27 +0300

    first get on page

--- src/components/Recipes/Recipes.jsx
+++ src/components/Recipes/Recipes.jsx
@@ -1,15 +1,16 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useStore } from "../../zustand/store";
 import Recipe from "./Recipe/Recipe";
 import './repices.scss'
 
 
 const Recipes = () => {
+    const [page,setPage]=useState(1)
     const fetchData=useStore((state)=>state.fetchData)
-    useEffect(() => {
-        fetchData()
-    });
     const recipes = useStore((state)=>state.data);
+    useEffect(() => {
+        if(recipes.length===0){fetchData(page)}
+    });
     return (
         <div className="repices">
             {recipes.map((el) => (
--- src/zustand/store.js
+++ src/zustand/store.js
@@ -2,25 +2,23 @@ import { create } from "zustand";
 
 export const useStore = create((set) => ({
     recipes: [],
-    //     deleteRecipes:(id)=>set(state=>{
-    //       const updatedRecipes = state.recipes.filter((el) => !id.includes(recipe.id))
-    //       return {recipes:[...updatedRecipes]}
-    //     }),
     data: [],
-    fetchData: async () => {
-        const res = await fetch("https://api.punkapi.com/v2/beers?page=1");
-        const newData = await res.json();
-        set({
-            data: newData.map(
-                ({ id, name, tagline, description, image_url }) => ({
-                    id,
-                    name,
-                    tagline,
-                    description,
-                    image_url,
-                })
-            ),
+    fetchData: async (page) => {
+        const res = await fetch(
+            `https://api.punkapi.com/v2/beers?page=${page}`
+        );
+        const result = await res.json();
+        const newRecipes = result.map(
+            ({ id, name, tagline, description, image_url }) => ({
+                id,
+                name,
+                tagline,
+                description,
+                image_url,
+            })
+        );
+        set(state=>{
+             return {data:[...state.data,...newRecipes]}
         });
     },
-
 }));
